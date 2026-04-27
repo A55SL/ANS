a6875a03f2ec99163329dc5c02c6fbe342c92aae
A55SL <alislarki@gmail.com>
2026-04-27 11:14:12 +0200

    add description of script (#4)
    * add description of script
    
    * extra code added

--- sql.py
+++ sql.py
@@ -1,3 +1,15 @@
-
+#Simple Python script
 print("Hello, World!")
 x = 5 + 3
+print("5 + 3 =", x)
+
+name = "Ali"
+print("Hello,", name)
+
+numbers = [1, 2, 3, 4, 5]
+total = sum(numbers)
+print("Sum of", numbers, "is", total)
+
+for i in range(3):
+    print("Loop iteration:", i)
+
